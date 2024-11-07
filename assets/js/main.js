c1d3be4ff2c20c877f64a7bba935acef820e4d72
Tran Dinh Khanh Du <trandinhkhanhdu2005@gmail.com>
2024-11-08 01:55:12 +0700

    slider

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,16 @@
-alert("javascript alert")
+var slider_length = document.querySelector(".slider__box img").offsetWidth;
+document.querySelector(".slider__box").style.width = `${slider_length}px`;
+var slider_img_counts = document.querySelectorAll(".slider__box__imgs img").length;
+var c = 1;
+function sliderChange () {
+    document.querySelector(".slider__box__imgs").style.transform = `translateX(${slider_length*-1*c}px)`;
+    c = c+1;
+    if (c == slider_img_counts) {
+        // document.querySelector(".slider__box__imgs").style.transition = "unset" ;
+        c = 0;
+    }
+}
+
+setInterval(() => {
+    sliderChange();
+}, 2000);
